af32ec326c60e8917eae4490353115852b7189b7
ANTOINE Lucas <rsma.simplon21@gmail.com>
2021-04-08 08:52:15 -0400

    portfolio antoine

---
+++ antoine/index.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>Portfolio ANTOINE</title>
+  </head>
+  <body>
+    <div class="container marginTop">
+        <div class="row">
+            <div class="col-6">
+                <h2>Portfolio</h2>
+               
+            </div>
+            <div class="col-6">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                      <ul class="navbar-nav">
+                        <li class="nav-item active">
+                          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                              <li class="nav-item active">
+                                <a class="nav-link marg" href="#">Work <span class="sr-only">(current)</span></a>
+                              </li>
+                              <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                                <ul class="navbar-nav">
+                                  <li class="nav-item active">
+                                    <a class="nav-link marg" href="#">Blog <span class="sr-only">(current)</span></a>
+                                  </li>
+                                  <button type="button" class="btn btn-outline-primary marg border">Hire Me!</button>
+                        
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </nav>
+            </div>
+        </div>
+    </div>
+
+                        
+                                   <div class="row backG">
+                                    <img src="img/me.jpg" alt="" id="resizePic">
+                                
+                                <div class="col-6">
+                                        <h3 class="hello">Hello i'm Lucas</h3>
+                                        <h1 class="txt">Expert in Graphic</h1>
+                                        <h1 class="txt">Design &Development</h1>
+                                        <p class="txt">Currently available for select projects, collaboration and consulting.Curently <br>
+                                        available for select projects. collaboration and consulting.
+                                        </p>
+                                        <button type="button" class="btn btn-primary border txt">SEE MY WORK</button>
+                                </div>
+                            </div>
+
+
+                            <h4 class="placeH">S E R V I C E S</h4>
+
+                            <div class="container">
+                                <div class="row">
+                                  <div class="col-4">
+                                    <button class="tailleS removeBorder btn"><img src="img/icone_pot.png"><p class="up bold">WEB DESIGN</p> <hr class="" >  <p>Well we're movin'on up to the east <br>side To a deluxe apartment in the</p></button>
+                                  </div>
+                                  <div class="col-4">
+                                    <button class="tailleS removeBorder btn bg"><img src="img/icone_diamant.png"><p class="up white bold">MOBILE APP</p><hr class="hr" > <p class="white">Tracking immunizations has never <br>been easier or more secure...</p></button>
+                                  </div>
+                                  <div class="col-4">
+                                    <button class="tailleS removeBorder btn"><img src="img/icone_batton_magic.png"><p class="up bold">BARNDING</p> <hr class="" ><p>Well we're movin'on up to the east <br>side To a deluxe apartment in the</p></button>
+                                  </div>
+                                </div>
+                            </div>
+
+
+
+
+                            <h4 class="placeH margTop" >P O R T F O L I O </h4>
+
+
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <img src="img/image_projet.jpg" alt="" class="imgSize">
+                                    </div>
+                                    <div class="col-4">
+                                        <img src="img/image_projet.jpg" alt="" class="imgSize">
+                                    </div>
+                                    <div class="col-4">
+                                        <img src="img/image_projet.jpg" alt="" class="imgSize">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="container up">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <img src="img/image_projet.jpg" alt="" class="imgSize">
+                                    </div>
+                                    <div class="col-4">
+                                        <img src="img/image_projet.jpg" alt="" class="imgSize">
+                                    </div>
+                                    <div class="col-4">
+                                        <img src="img/background_titre_theme_projet.png" alt="" class="imgSize">
+                                        <div class="caption">
+                                            <p class="imgOnTxt">Home deco</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+
+
+
+                               
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
