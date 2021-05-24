85a6e0f9639a58b84905dedd639fdecbb54be890
ANTOINE Lucas <rsma.simplon21@gmail.com>
2021-05-24 19:22:02 -0400

    evaluation stage le date du jour

--- rsma-antoine.gp/includes/main.php
+++ rsma-antoine.gp/includes/main.php
@@ -1,108 +1,199 @@
-<main class="container">
-    <div class="row">
-        <div class="col-md-4">
+<body>
+    <div class="page-wrapper bg-gra-03 p-t-45 p-b-50">
+        <div class="wrapper wrapper--w790">
+            <div class="card-5">
+                <div class="card-heading">
+                    <h2 class="title">Formulaire d'inscription</h2>
+                </div>
+                <div class="card-body">
+                    <form method="POST" action="traitement.php" class="needs-validation" novalidate>
+                        <div class="form-row">
+                            <div class="name">Civilité (*)</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                        <select name="civilite" required>
+                                            <option disabled="disabled" selected="selected" value="">Choisissez votre civilité :
+                                            </option>
+                                            <option value="monsieur">Monsieur</option>
+                                            <option value="madame">Madame</option>
+                                            <option value="mademoiselle">Mademoiselle</option>
+                                        </select>
+                                        <div class="select-dropdown"></div>
+                                        <div class="valid-feedback">
+                                            champ Valide !
+                                        </div>
+                                        <div class="invalid-feedback">
+                                            Veuillez selectionner une civilité
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row m-b-55">
+                            <div class="name">Nom (*)</div>
+                            <div class="value">
+                                <div class="row row-space">
+                                    <div class="col-6">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="text" name="prenom" maxlength="55" placeholder="Prénom" required>
+                                            <div class="valid-feedback">
+                                                champ Valide !
+                                            </div>
+                                            <div class="invalid-feedback">
+                                                Veuillez entrer un prénom
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="text" name="nom" maxlength="100" placeholder="NOM" required>
+                                            <div class="valid-feedback">
+                                                champ Valide !
+                                            </div>
+                                            <div class="invalid-feedback">
+                                                Veuillez entrer un nom
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Téléphone (*)</div>
+                            <div class="value">
+                                <div class="row row-refine">
+                                    <div class="col-9">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="text" name="telephone" maxlength="15" required>
+                                            <div class="valid-feedback">
+                                                champ Valide !
+                                            </div>
+                                            <div class="invalid-feedback">
+                                                Veuillez entrer un numéro de téléphone
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
 
-        </div>
-        <div class="col-md-4">
-            <form class="needs-validation" novalidate>
-                <div class="form-group">
-                    <label for="pays">Civilite</label>
-                    <select class="form-control" id="pays" required>
-                        <option selected>Precisez votre civilite</option>
-                        <option>Monsieur</option>
-                        <option>Madame</option>
-                    </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Email (*)</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="email" name="email" maxlength="160" required>
+                                    <div class="valid-feedback">
+                                        champ Valide !
+                                    </div>
+                                    <div class="invalid-feedback">
+                                        Veuillez entrer une adresse email
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Mot de passe</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="password" name="motdepasse" minlength="6" maxlength="20">
+                                    <div class="valid-feedback">
+                                        champ Valide !
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Adresse</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="text" name="adresse" maxlength="60">
+                                    <div class="valid-feedback">
+                                        champ Valide !
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Adresse complémentaire</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="text" name="complementaire">
+                                    <div class="valid-feedback">
+                                        champ Valide !
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Code Postal</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="text" name="postal" maxlength="5">
+                                    <div class="valid-feedback">
+                                        champ Valide !
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Ville</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="text" name="ville" maxlength="40">
+                                    <div class="valid-feedback">
+                                        champ Valide !
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Pays</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                        <select name="pays">
+                                            <option disabled="disabled" selected="selected" value="">Choisissez votre pays :</option>
+                                            <option value="guadeloupe">Guadeloupe</option>
+                                            <option value="martinique">Martinique</option>
+                                            <option value="reunion">La Réunion</option>
+                                            <option value="france">France</option>
+                                        </select>
+                                        <div class="valid-feedback">
+                                            champ Valide !
+                                        </div>
+                                        <div class="select-dropdown"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <button class="btn btn--radius-2 btn--red" style="background: #ff4b5a;" type="submit">Enregistrer</button>
+                        </div>
+                    </form>
+                    <script>
+                    // Example starter JavaScript for disabling form submissions if there are invalid fields
+                    (function() {
+                        'use strict';
+                        window.addEventListener('load', function() {
+                            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                            var forms = document.getElementsByClassName('needs-validation');
+                            // Loop over them and prevent submission
+                            var validation = Array.prototype.filter.call(forms, function(form) {
+                                form.addEventListener('submit', function(event) {
+                                    if (form.checkValidity() === false) {
+                                        event.preventDefault();
+                                        event.stopPropagation();
+                                    }
+                                    form.classList.add('was-validated');
+                                }, false);
+                            });
+                        }, false);
+                    })();
+                    </script>
                 </div>
-                <div class="form-group">
-                    <label for="">NOM</label>
-                    <input type="<text" class="form-control" id="nom" placeholder="votre NOM"
-                        maxlength="100" required>
-                    <div class="valid-feedback">
-                        Bon !
-                    </div>
-                    <div class="invalid-feedback">
-                        Indiquez votre nom !
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="">Prenom</label>
-                    <input type="<text" class="form-control" id="prenom" placeholder="votre Prenom"
-                        maxlength="55" required>
-                    <div class="valid-feedback">
-                        Bon !
-                    </div>
-                    <div class="invalid-feedback">
-                        Indiquez votre prénom !
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="">telephone</label>
-                    <input type="<tel" class="form-control" id="telephone"
-                        placeholder="votre numero de telephone" maxlength="15" required>
-                    <div class="valid-feedback">
-                        Bon !
-                    </div>
-                    <div class="invalid-feedback">
-                        Indiquez votre numero de telephone !
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="">Email address</label>
-                    <input type="email" class="form-control" id="email"
-                        placeholder="name@example.com" maxlength="160" required>
-                    <div class="valid-feedback">
-                        Bon !
-                    </div>
-                    <div class="invalid-feedback">
-                        Indiquez votre adresse email !
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="">Mot de passe</label>
-                    <input type="password" class="form-control" id="password"
-                        placeholder="votre Mot de passe" maxlength="20" required>
-                    <div class="valid-feedback">
-                        Bon !
-                    </div>
-                    <div class="invalid-feedback">
-                        Indiquez votre mot de passe !
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="">Adresse</label>
-                    <input type="<text" class="form-control" id="adresse" placeholder="votre Adresse"
-                        maxlength="60">
-                </div>
-                <div class="form-group">
-                    <label for="">Adresse complementaire</label>
-                    <input type="<text" class="form-control" id="adresseComp"
-                        placeholder="complement d'adresse">
-                </div>
-                <div class="form-group">
-                    <label for="">Code postal</label>
-                    <input type="<text" class="form-control" id="codePostal"
-                        placeholder="votre Code postal" maxlength="5">
-                </div>
-                <div class="form-group">
-                    <label for="">Ville</label>
-                    <input type="<text" class="form-control" id="ville" placeholder="votre Ville"
-                        maxlength="40">
-                </div>
-                <div class="form-group">
-                    <label for="pays">Pays</label>
-                    <select class="form-control" id="pays">
-                        <option selected>Selectionner un pays</option>
-                        <option>Guadeloupe</option>
-                        <option>Martinique</option>
-                        <option>La réunion</option>
-                        <option>France</option>
-                    </select>
-                </div>
-                <button type="submit" class="btn btn-success">Valider</button>
-            </form>
-        </div>
-        <div class="col-md-4">
-
+            </div>
         </div>
     </div>
-</main>
--- rsma-antoine.gp/index.php
+++ rsma-antoine.gp/index.php
@@ -1,33 +1,12 @@
-<?php  include("includes/header.php") ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-<?php  include("includes/main.php") ?>
-   
 
-<?php  include("includes/footer.php") ?>
-    <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
-    </script>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
 
-    <script>
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function() {
-        'use strict';
-        window.addEventListener('load', function() {
-            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-            var forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function(form) {
-                form.addEventListener('submit', function(event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                    form.classList.add('was-validated');
-                }, false);
-            });
-        }, false);
-    })();
-    </script>
-</body>
-
-</html>
+            <a href="https://rsma-antoine.gp/ajouter_salarier.php"><button type="button"
+                    class="btn btn-primary">ajouter un salarié</button></a>
+        </div>
+    </div>
+</div>
